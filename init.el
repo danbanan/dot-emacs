979b9a06c070d1a1d9458516ab458024d62da916
Dan Rachou <dan.rachou@gmail.com>
2020-03-11 10:02:18 +0100

    added new modes

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 ;; BASIC SETUP
 (require 'package)
+
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+	'("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless (file-exists-p (expand-file-name
@@ -9,11 +10,19 @@
 				 "/archives/MELPA/")))
   (package-refresh-contents))
 
-
-;; Install theme package
+;; THEMES
 (unless (package-installed-p 'alect-themes)
   (package-install 'alect-themes))
 
+(unless (package-installed-p 'ample-theme)
+  (package-install 'ample-theme))
+
+(unless (package-installed-p 'gruvbox-theme)
+  (package-install 'gruvbox-theme))
+
+;; CENTER WINDOW: activated only on single windows
+(unless (package-installed-p 'centered-window)
+  (package-install 'centered-window))
 
 ;; FRAME SETUP
 (add-hook 'window-setup-hook
@@ -22,23 +31,43 @@
 		  ns-pop-up-frames nil
 		  mac-command-modifier nil
 		  select-enable-clipboard t)
-	    (set-face-attribute 'default nil :family "Anonymous Pro" :height 170)
+	    (set-face-attribute 'default nil :family "Source Code Pro" :height 160)
 	    (tool-bar-mode -1)
 	    (scroll-bar-mode -1)
+	    (set-frame-parameter nil 'undecorated t)
 	    (set-frame-position nil 0 0)
 	    (set-frame-size nil (- (display-pixel-width) 20) (display-pixel-height) t)
-	    (set-frame-parameter nil 'undecorated t)
-	    ;; Awesome color theme
-	    (load-theme 'alect-light)))
+	    (centered-window-mode t)
+	    (global-visual-line-mode t)
+	    ;; (load-theme 'alect-light-alt)
+	    ;; (load-theme 'ample-light)
+	    (load-theme 'gruvbox-light-soft)))
+
+;; Show line numbers
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode t))
+
+;; Set line numbers to visually relative
+(setq-default display-line-numbers-type 'visual)
+
+;; ORG-MODE
+(unless (package-installed-p 'org)
+  (package-install 'org))
+
+;; Beautiful bullets in org-mode
+(unless (package-installed-p 'org-bullets)
+  (package-install 'org-bullets))
+
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 
 
-;; CC MODE:
+;; CC-MODE
 (require 'cc-mode)
 
 ;; Set coding style
 (setq c-default-style '((java-mode . "java")
-			(cc-mode . "linux")
+			(cc-mode . "k&r")
 			(other . "gnu")))
 
 
@@ -56,8 +85,9 @@
 ;; Showing matching parantheses
 (show-paren-mode 1)
 
-;; Loads supercool old school color theme
-;; TODO: Get rid of annyoing startup questions. Install manually.
+
+
+
 
 
 ;; Function to set frame to half screen
@@ -88,10 +118,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" default)))
+    ("a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "123a8dabd1a0eff6e0c48a03dc6fb2c5e03ebc7062ba531543dfbce587e86f2a" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" default)))
  '(package-selected-packages
    (quote
-    (alec-themes geiser markdown-mode dracula-theme auctex alect-themes))))
+    (centered-window org-bullets org ample-theme alec-themes geiser markdown-mode dracula-theme auctex alect-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
