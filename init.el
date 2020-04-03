acae86fc4d0543a6565b82770eb8ea814c36e484
Dan Rachou <dan.rachou@gmail.com>
2020-04-03 22:28:32 +0200

    stuff for markdown, ledger and org-mode

--- init.el
+++ init.el
@@ -80,10 +80,22 @@
 ;; Fontify the whole line for headings (with a background color). - Leuven theme
 (setq org-fontify-whole-heading-line t)
 ;; Agenda config
-(setq org-agenda-files '("/Users/danrachou/OneDrive/Documents/org/planner"))
+(setq-default org-agenda-files '("~/Dropbox/org/planner"))
 (setq org-agenda-skip-scheduled-if-deadline-is-shown 'repeated-after-deadline)
 ;; Makes sure org is loaded
 (setq org-log-done t)
+;; Adding habits to org modules
+(add-to-list 'org-modules 'org-habits)
+
+;; MARKDOWN-MODE
+(unless (package-installed-p 'markdown-mode)
+  (package-install 'markdown-mode))
+
+;; LEDGER-MODE
+(unless (package-installed-p 'ledger-mode)
+  (package-install 'ledger-mode))
+;; Load ledger-mode for '.dat' files
+(add-to-list 'auto-mode-alist '("\\.dat\\'" . ledger-mode))
 
 ;; CC-MODE
 (require 'cc-mode)
@@ -140,12 +152,14 @@
 
 ;; Function to set frame to half screen
 (defun halfscreen-frame ()
+  (interactive)
   (set-frame-size nil (- (/ (display-pixel-width) 2) 20)
 		  (display-pixel-height) t))
 
 ;; Function to set frame to full screen
 (defun fullscreen-frame ()
-  (set-frame-size nil (- (display-pixel-width) 20) (display-pixel-height)))
+  (interactive)
+  (set-frame-size nil (- (display-pixel-width) 20) (display-pixel-height) t))
 
 ;; (defun my-asm-mode-hook ()
 ;;   ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
@@ -167,9 +181,16 @@
  '(custom-safe-themes
    (quote
     ("a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "123a8dabd1a0eff6e0c48a03dc6fb2c5e03ebc7062ba531543dfbce587e86f2a" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "5e3fc08bcadce4c6785fc49be686a4a82a356db569f55d411258984e952f194a" "7153b82e50b6f7452b4519097f880d968a6eaf6f6ef38cc45a144958e553fbc6" default)))
+ '(ledger-reports
+   (quote
+    (("netw" "ledger balance Assets Liabilities")
+     ("bal" "%(binary) -f %(ledger-file) bal")
+     ("reg" "%(binary) -f %(ledger-file) reg")
+     ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+     ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
  '(package-selected-packages
    (quote
-    (centered-window org-bullets org ample-theme alec-themes geiser markdown-mode dracula-theme auctex alect-themes))))
+    (ledger-mode centered-window org-bullets org ample-theme alec-themes geiser markdown-mode dracula-theme auctex alect-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
