706bac16db7ae6d5eadfdaefefc278fd0bbda267
danbanan <dan.rachou@gmail.com>
2021-04-28 09:19:10 +0200

    Tomorrow theme is nice

--- themes.el
+++ themes.el
@@ -1,6 +1,5 @@
-;; THEMES
-(unless (package-installed-p 'alect-themes)
-  (package-install 'alect-themes))
+(use-package alect-themes
+  :ensure t)
 
 (unless (package-installed-p 'ample-theme)
   (package-install 'ample-theme))
@@ -11,10 +10,38 @@
 (use-package github-modern-theme
   :ensure t)
 
-;; Current theme
 (use-package solarized-theme
-  :ensure t
-  :init (load-theme 'solarized-dark t))
+  :ensure t)
 
 (use-package leuven-theme
   :ensure t)
+
+;; (use-package spacemacs-theme
+;;   :ensure t)
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t)
+
+(defvar current-theme nil)
+
+;; (defun load-light-theme ()
+;;   (interactive)
+;;    (load-theme 'solarized-light-high-contrast t)
+;;    (setq current-theme 'solarized))
+
+;; (defun load-dark-theme ()
+;;   (interactive)
+;;    (load-theme 'solarized-dark t)
+;;    (setq current-theme 'solarized))
+
+(defun load-light-theme ()
+  (interactive)
+   (load-theme 'sanityinc-tomorrow-day t)
+   (setq current-theme 'tomorrow))
+
+(defun load-dark-theme ()
+  (interactive)
+   (load-theme 'sanityinc-tomorrow-night t)
+   (setq current-theme 'tomorrow))
+
+(load-light-theme)
