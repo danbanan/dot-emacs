dae6cbef447c07ab4602e39be0a334ada4c17a03
Dan Rachou <dan.rachou@gmail.com>
2021-05-22 17:20:53 +0200

    Add code completion to setup

--- setups.el
+++ setups.el
@@ -9,6 +9,7 @@
   (vterm-insert "ssh x86-1")
   (vterm-send-return)
   ;; setup windows
+  (bookmark-jump "tegra-machine")
   (bookmark-jump "in5050-sisci")
   (split-window-right)
   (switch-to-buffer-other-window "x86-vterm")
@@ -18,5 +19,10 @@
   ;; set compilation
   (setq compile-command "./run.sh --tegra tegra-2 --clean")
   ;; store window setup for easier jumping
-  (window-configuration-to-register 1))
-  
+  (window-configuration-to-register 1)
+  ;; Completion settings
+  (add-to-list 'company-c-headers-path-system "/ssh:in5050:/usr/include/")
+  (add-to-list 'company-c-headers-path-system "/ssh:tegra-2:/opt/DIS/include/")
+  (add-to-list 'company-c-headers-path-system "/ssh:tegra-2:/opt/DIS/include/dis/")
+  (add-to-list 'company-c-headers-path-system "/ssh:tegra-2:/opt/DIS/include/os/"))
+
